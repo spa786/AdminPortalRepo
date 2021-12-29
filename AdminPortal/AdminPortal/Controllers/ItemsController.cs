3d533d3acceb54beb5cccf821012bad9544dfa02
Sunil Pal <palsunil308@yahoo.in>
2021-12-29 22:06:34 +0530

    abc

---
+++ AdminPortal/AdminPortal/Controllers/ItemsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetRecord()
+        {
+            return Ok("Hello Admin Portal");
+        }
+    }
+}
---
+++ AdminPortal/AdminPortal/Model/ItemMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminPortal.Model
+{
+    public class ItemMaster
+    {
+        //Data Annotations 
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Item Name Required")]
+        [MaxLength(50)]
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public bool Status { get; set; }
+    }
+}
